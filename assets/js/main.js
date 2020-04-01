cc62361ee22a8b1127e25ad471ba80b116c392bb
Ryan Kelley <rdkelley@users.noreply.github.com>
2020-03-31 18:32:31 -0700

    feat(basic): add sorting by date, draggable cards
    Co-authored-by: meghedi <hacopian.m@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,52 @@ const DATE_FORMAT = {
   day: "numeric"
 };
 
+const isSameDate = (date1, date2) => {
+  console.log(date1, date2);
+
+  return (
+    date1.getFullYear() == date2.getFullYear() &&
+    date1.getMonth() == date2.getMonth() &&
+    date1.getDate() == date2.getDate()
+  );
+};
+
+let taskState = [];
+
+const renderTaskLists = () => {
+  taskState.forEach(element => {
+    console.log(element.dueDate);
+
+    let innerHtml = `
+      <li class="task">
+        <h1>${element.title}</h1>
+        <p>${element.description ? element.description : ""}</p>
+        <p>Due: ${new Date(element.dueDate.$date).toLocaleString(
+          "en-US",
+          DATE_FORMAT
+        )}</p>
+      </li>`;
+
+    // If today is same date as duedate, render to today's list
+    if (isSameDate(new Date(), new Date(element.dueDate.$date))) {
+      $(".taskList-today").append(innerHtml);
+    } else {
+      $(".taskList-future").append(innerHtml);
+    }
+  });
+
+  $("#sortable1, #sortable2")
+    .sortable({
+      connectWith: ".connectedSortable",
+      containment: ".taskList-container"
+    })
+    .disableSelection();
+};
+
 $(document).ready(() => {
   API.getAllTasks().then(function(data) {
-    data.forEach(element => {
-      console.log(element.dueDate);
-      let innerHtml = `
-        <div class="task">
-          <h1>${element.title}</h1>
-          <p>${element.description ? element.description : ""}</p>
-          <p>Due: ${new Date(element.dueDate.$date).toLocaleString(
-            "en-US",
-            DATE_FORMAT
-          )}</p>
-        </div>`;
-      $(".taskList").append(innerHtml);
-    });
+    taskState = data;
+
+    renderTaskLists();
   });
 });
